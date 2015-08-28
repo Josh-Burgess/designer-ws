a156d8238176cdda3bbece061027e333aae8d7c1
Josh-Burgess <joshua.k.burgess@gmail.com>
2015-08-28 01:28:47 -0500

    initial checkin

---
+++ README.md
@@ -0,0 +1,17 @@
+Designer Work Sample
+====================
+
+Design and implement a landing page for [Pied Piper](http://www.hbo.com/silicon-valley) — everyone’s favorite fictional startup — using HTML, CSS, and (optionally) JavaScript. The goals for this landing page are to create brand awareness and convince potential customers to become actual customers.
+
+#### Things to Know
+
+* We’ve provided content, basic markup, as well as assets to get you started.
+* All content must remain intact, but all markup can be modified, substituted, or supplemented.
+* All assets can be modified, substituted, or supplemented — even the logo. Go wild.
+* This project will in no way be used by anyone, ever, and completely belongs to you (and HBO).
+
+#### Getting Started
+
+* [Download this project](https://github.com/compose/designer-ws/archive/master.zip) or fork it if you prefer.
+* Use voodoo, magic, or telepathy to complete and implement your design.
+* Zip up your source files and submit them through Compose’s [application site](https://jobs.compose.io/jobs/openings/designer).
---
+++ javascripts/scripts.js
@@ -0,0 +1,59 @@
+// Scripts
+document.addEventListener("DOMContentLoaded", function () {
+  Array.prototype.slice.call(document.querySelectorAll('.nav-toggle')).forEach(function (toggle) {
+    toggle.addEventListener('click', add_active);
+  })
+
+  function add_active(e) {
+    this.classList.toggle('active');
+  }
+
+  var header = document.querySelector('body > header'),
+      offset = header.offsetTop;
+  document.addEventListener("scroll", function (e) {
+    if (this.scrollingElement.scrollTop > offset) {
+      header.classList.add("fix");
+    } else {
+      header.classList.remove("fix");
+    }
+  });
+
+  setTimeout((function () {
+    var hidden = "hidden";
+
+    // Standards:
+    if (hidden in document)
+      document.addEventListener("visibilitychange", onchange);
+    else if ((hidden = "mozHidden") in document)
+      document.addEventListener("mozvisibilitychange", onchange);
+    else if ((hidden = "webkitHidden") in document)
+      document.addEventListener("webkitvisibilitychange", onchange);
+    else if ((hidden = "msHidden") in document)
+      document.addEventListener("msvisibilitychange", onchange);
+      // IE 9 and lower:
+    else if ("onfocusin" in document)
+      document.onfocusin = document.onfocusout = onchange;
+      // All others:
+    else
+      window.onpageshow = window.onpagehide
+      = window.onfocus = window.onblur = onchange;
+
+    function onchange(evt) {
+      var v = "visible", h = "hidden",
+          evtMap = {
+            focus: v, focusin: v, pageshow: v, blur: h, focusout: h, pagehide: h
+          };
+
+      evt = evt || window.event;
+      if (evt.type in evtMap)
+        document.body.className = evtMap[evt.type];
+      else
+        document.body.className = this[hidden] ? "hidden" : "visible";
+    }
+
+    // set the initial state (but only if browser supports the Page Visibility API)
+    if (document[hidden] !== undefined)
+      onchange({ type: document[hidden] ? "blur" : "focus" });
+  }), 100);
+});
+
